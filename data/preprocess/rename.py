86db4209369d8826ac72329b592ec0254c9bde6b
daidalos99 <daidalos0430@gmail.com>
2023-10-20 16:27:12 +0900

    split test from train

--- data/preprocess/rename.py
+++ data/preprocess/rename.py
@@ -1,14 +1,12 @@
 import os
 
-folder_path = "./data/train/augmentation/labelIds"
-#folder_path = "./val/labelIds"
-#folder_path = "./val/leftImg"
+folder_path = "C:/Users/daida/Desktop/CityScapes_Segmentation_Project/data/dataset/test/leftImg"
 
 new_prefix = ""  # Specify the new prefix for the filenames
 
-counter = 5951
+counter = 1 # 시작 숫자 몇번부터?
 for filename in os.listdir(folder_path):
-    if filename.endswith(".png"):
-        new_filename = f"{new_prefix}{counter:05d}.png"
+    if filename.endswith(".jpg"):
+        new_filename = f"{new_prefix}{counter:03d}.jpg" # 숫자 이름 몇 자리로 할건지
         os.rename(os.path.join(folder_path, filename), os.path.join(folder_path, new_filename))
         counter += 1
